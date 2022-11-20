12077366801ebee22ef3cc88f668f50f43a37d94
Wiktor <wykwit@disroot.org>
2022-11-20 16:17:37 +0100

    Update preziscraper.js

--- preziscraper.js
+++ preziscraper.js
@@ -1,96 +1,65 @@
-const puppeteer = require('puppeteer-core');
+#!/bin/env node
+
+const fs = require('fs');
+
 const argv = require('minimist')(process.argv.slice(2));
-var fs = require('fs');
+const puppeteer = require('puppeteer');
 
-var prezi = null;
-var out = "img";
-
-// set prezi url
-if("url" in argv){
-    prezi= argv.url;
-} else{
-    throw "PREZI nOT FONT"
+// args
+var prezi = argv.url;
+var chrome = ('chrome' in argv) ? argv.chrome : '/usr/bin/chromium';
+var height = ('height' in argv) ? argv.height : 720;
+var width = ('width' in argv) ? argv.width : 1280;
+var wait = ('wait' in argv) ? argv.wait : 1200;
+var out = ('out' in argv) ? argv.out : 'out';
+if (!fs.existsSync(`./${out}`)) {
+	fs.mkdirSync(`./${out}`);
 }
 
-if("out" in argv){
-    out = argv.out;
+const button = {
+	start: '.viewer-common-info-overlay-button-icon',
+	fullscreen: '.webgl-viewer-navbar-fullscreen-enter-icon',
+	cookies: '.onetrust-close-btn-ui',
+	next: '.webgl-viewer-navbar-next-icon',
+	stop: '.webgl-viewer-navbar-ellipsis-icon',
 }
 
-if (!fs.existsSync(`./${out}`)){
-    fs.mkdirSync(`./${out}`);
+async function main() {
+	const browser = await puppeteer.launch({ executablePath: chrome, headless: false });
+	const page = await browser.newPage();
+	console.log('browser started...');
+
+	await page.setViewport({
+		width: width,
+		height: height,
+		deviceScaleFactor: 1,
+	});
+	console.log('viewport set...');
+
+	await page.goto(prezi);
+	console.log('prezi url loaded...');
+
+	for (var label of ['start', 'cookies', 'fullscreen']) {
+		await page.waitForSelector(button[label]);
+		await page.click(button[label]);
+		console.log(label + ' button clicked ...');
+	}
+
+	for (var i = 1; ; i++) {
+		await page.mouse.move(100, 100);
+		await page.waitForTimeout(wait);
+		console.log(`loading slide ${i}...`)
+		await page.screenshot({path: `${out}/prezi-${i}.png`});
+		console.log('screenshot saved...')
+		if (await page.$(button.stop) !== null) break;
+		await page.waitForSelector(button.next);
+		await page.click(button.next);
+	}
+
+	console.log('done...');
+	await browser.close();
+	console.log('browser closed...')
 }
 
-// uncomment if you want to emulate different devices
-// const devices = require('puppeteer-core/DeviceDescriptors');
-// const iPad = devices['iPad Pro'];
+main();
 
-// default values
-if(process.platform === "win32"){
-    var chromePath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
-} else {
-    var chromePath = "/usr/bin/google-chrome";
-}
-var height = 842;
-var width = 595;
-
-// overwrite default if present
-if("chromePath" in argv){
-    chromePath = argv.chromePath;
-}
-
-if("width" in argv){
-    width = argv.width
-}
-
-if("height" in argv){
-    height = argv.height
-}
-
-// prezi control
-const fullscr = ".webgl-viewer-navbar-fullscreen-enter-icon";
-const nxt = ".webgl-viewer-navbar-next-icon";
-const breaker = ".viewer-common-info-overlay-button-label";
-
-(async () => {
-    const browser = await puppeteer.launch(
-        {
-            executablePath: chromePath,
-            headless:true
-        }
-    );
-    const page = await browser.newPage();
-    // await page.emulate(iPad);
-    await page.setViewport({
-        width: width,
-        height: height,
-        deviceScaleFactor: 1,   
-    });
-    await page.goto(prezi,{
-        timeout: 3000000
-    });
-
-    n = 0;
-    // TODO: Refactor code into promise
-    await page.waitForSelector(fullscr);
-    await page.click(fullscr);
-
-    // TODO: work on check if bar is existing
-    // await page.waitFor(() => !document.querySelector(nxt));
-    await page.screenshot({path: `img/prezi-${n}.png`});
-    await page.mouse.move(100, 100);
-    await page.waitForSelector(".webgl-viewer-navbar-next-icon");
-    await page.click(".webgl-viewer-navbar-next-icon");
-
-    while(true){
-        // await page.waitFor(() => !document.querySelector(nxt));
-        n++;
-        await page.waitFor(1200);
-        if (await page.$(breaker) !== null) break;
-        await page.screenshot({path: `img/prezi-${n}.png`});
-        await page.mouse.move(100, 100);
-        await page.waitForSelector(nxt);
-        await page.click(nxt);
-    }
-
-    await browser.close();
-})();
